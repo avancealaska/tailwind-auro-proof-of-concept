19e32776dd13d87b1f2133dec4ff40f657ee7406
Andrew Vance <andrew.vance@alaskaair.com>
2021-10-22 14:28:19 -0700

    added basic auro styles

--- src/App.js
+++ src/App.js
@@ -3,21 +3,119 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <section>
+        <h2>Background colors</h2>
+        <ul>
+          <li>
+            <div className="bg-lightest">lightest</div>
+          </li>
+          <li>
+            <div className="bg-lighter">lighter</div>
+          </li>
+          <li>
+            <div className="bg-darker">darker</div>
+          </li>
+          <li>
+            <div className="bg-darkest">darkest</div>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Border colors</h2>
+        <ul>
+          <li>
+            <div className="border border-primary-on-light">
+              primary-on-light
+            </div>
+          </li>
+          <li>
+            <div className="border border-primary-on-dark">primary-on-dark</div>
+          </li>
+          <li>
+            <div className="border border-active-on-light">active-on-light</div>
+          </li>
+          <li>
+            <div className="border border-active-on-dark">active-on-dark</div>
+          </li>
+          <li>
+            <div className="border border-error-on-light">error-on-light</div>
+          </li>
+          <li>
+            <div className="border border-error-on-dark">error-on-dark</div>
+          </li>
+          <li>
+            <div className="border border-disabled-on-light">
+              disabled-on-light
+            </div>
+          </li>
+          <li>
+            <div className="border border-disabled-on-dark">
+              disabled-on-dark
+            </div>
+          </li>
+          <li>
+            <div className="border border-focus-on-light">focus-on-light</div>
+          </li>
+          <li>
+            <div className="border border-focus-on-dark">focus-on-dark</div>
+          </li>
+          <li>
+            <div className="border border-divider-on-light">
+              divider-on-light
+            </div>
+          </li>
+          <li>
+            <div className="border border-divider-on-dark">divider-on-dark</div>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Text color</h2>
+        <ul>
+          <li>
+            <span className="text-primary-on-light">primary-on-light</span>
+          </li>
+          <li>
+            <span className="text-primary-on-dark">primary-on-dark</span>
+          </li>
+          <li>
+            <span className="text-secondary-on-light">secondary-on-light</span>
+          </li>
+          <li>
+            <span className="text-secondary-on-dark">secondary-on-dark</span>
+          </li>
+          <li>
+            <span className="text-link-on-light">link-on-light</span>
+          </li>
+          <li>
+            <span className="text-link-on-dark">link-on-dark</span>
+          </li>
+          <li>
+            <span className="text-error-on-light">error-on-light</span>
+          </li>
+          <li>
+            <span className="text-error-on-dark">error-on-dark</span>
+          </li>
+          <li>
+            <span className="text-disabled-on-light">disabled-on-light</span>
+          </li>
+          <li>
+            <span className="text-disabled-on-dark">disabled-on-dark</span>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Font family</h2>
+        <ul>
+          <li>
+            <span className="font-default">default</span>
+          </li>
+          <li>
+            <span className="font-mono">mono</span>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,94 @@
+const auro = require('@alaskaairux/design-tokens/dist/tokens/JSObject--allTokens');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: auro.breakpoint.sm.value,
+      md: auro.breakpoint.md.value,
+      lg: auro.breakpoint.lg.value,
+    },
+    backgroundColor: {
+      lightest: auro.color.background.lightest.value,
+      lighter: auro.color.background.lighter.value,
+      darker: auro.color.background.darker.value,
+      darkest: auro.color.background.darkest.value,
+    },
+    borderColor: {
+      primary: {
+        on: {
+          light: auro.color.border.primary.onLight.value,
+          dark: auro.color.border.primary.onDark.value,
+        },
+      },
+      active: {
+        on: {
+          light: auro.color.border.active.onLight.value,
+          dark: auro.color.border.active.onDark.value,
+        },
+      },
+      error: {
+        on: {
+          light: auro.color.border.error.onLight.value,
+          dark: auro.color.border.error.onDark.value,
+        },
+      },
+      disabled: {
+        on: {
+          light: auro.color.border.disabled.onLight.value,
+          dark: auro.color.border.disabled.onDark.value,
+        },
+      },
+      focus: {
+        on: {
+          light: auro.color.border.Focus.onLight.value,
+          dark: auro.color.border.Focus.onDark.value,
+        },
+      },
+      divider: {
+        on: {
+          light: auro.color.border.divider.onLight.value,
+          dark: auro.color.border.divider.onDark.value,
+        },
+      },
+    },
+    textColor: {
+      primary: {
+        on: {
+          light: auro.color.text.primary.onLight.value,
+          dark: auro.color.text.primary.onDark.value,
+        },
+      },
+      secondary: {
+        on: {
+          light: auro.color.text.secondary.onLight.value,
+          dark: auro.color.text.secondary.onDark.value,
+        },
+      },
+      link: {
+        on: {
+          light: auro.color.text.link.onLight.value,
+          dark: auro.color.text.secondary.onDark.value,
+        },
+      },
+      error: {
+        on: {
+          light: auro.color.text.error.onLight.value,
+          dark: auro.color.text.error.onDark.value,
+        },
+      },
+      disabled: {
+        on: {
+          light: auro.color.text.disabled.onLight.value,
+          dark: auro.color.text.disabled.onDark.value,
+        },
+      },
+    },
+    fontFamily: {
+      default: auro.font.family.default.value,
+      mono: auro.font.family.mono.value,
+    },
   },
   variants: {
     extend: {},
